9f8373cf2f307c510f71345bdf0b79ce44c67507
LavnokLabs <LavnokLabs@users.noreply.github.com>
2020-06-11 17:18:59 +0530

    Video viewer changes

--- app/src/main/java/com/lavnok/yuj/ViewVideoFragment.java
+++ app/src/main/java/com/lavnok/yuj/ViewVideoFragment.java
@@ -2,19 +2,36 @@ package com.lavnok.yuj;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerSupportFragmentX;
+import com.lavnok.yuj.data.Videos;
+import com.lavnok.yuj.ui.videos.VideoViewModel;
+import com.lavnok.yuj.utils.Config;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class ViewVideoFragment extends Fragment {
+public class ViewVideoFragment extends Fragment{
+    final String TAG="com.lavnok.yuj-Logger";
+    VideoViewModel mViewModel;
+    Videos mVideo;
 
+    //youtube props
+    private static final int RECOVERY_REQUEST=1;
+    private YouTubePlayer youTubePlayer;
+
+//    private YouTubePlayerFragment youTubePlayerFragment;
 
     public ViewVideoFragment() {
         // Required empty public constructor
@@ -25,7 +42,36 @@ public class ViewVideoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_view_video, container, false);
+
+        View root = inflater.inflate(R.layout.fragment_view_video, container, false);
+
+        mViewModel = ViewModelProviders.of(this).get(VideoViewModel.class);
+        mVideo=mViewModel.getCurrentVideo();
+        YouTubePlayerSupportFragmentX youTubePlayerFragment = YouTubePlayerSupportFragmentX.newInstance();
+        FragmentTransaction transaction = getChildFragmentManager().beginTransaction();
+        youTubePlayerFragment.initialize(Config.YOUTUBE_API_KEY, new YouTubePlayer.OnInitializedListener() {
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                if(!b){
+                    youTubePlayer.cueVideo(mVideo.getVideoId());
+                }
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+                if (youTubeInitializationResult.isUserRecoverableError()) {
+                    youTubeInitializationResult.getErrorDialog(getActivity(),1).show();
+                } else {
+                    Toast.makeText(getActivity(),
+                            "YouTubePlayer.onInitializationFailure(): " + youTubeInitializationResult.toString(),
+                            Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+        transaction.replace(R.id.frame_fragment, youTubePlayerFragment).commit();
+
+        return root;
     }
 
 }
---
+++ app/src/main/java/com/lavnok/yuj/utils/Config.java
@@ -0,0 +1,8 @@
+package com.lavnok.yuj.utils;
+
+public final class Config {
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyAkoH5HVlIcSJXFxukFfZv4nb6IWRRon8c";
+}
